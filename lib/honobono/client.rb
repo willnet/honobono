b30ac9098c8850b3ba3d5a5149ba4ecb9eb53bc8
willnet <netwillnet@gmail.com>
2016-08-30 16:47:01 +0900

    :cosme:

--- lib/honobono/client.rb
+++ lib/honobono/client.rb
@@ -4,14 +4,20 @@ module Honobono
       new.run
     end
 
+    def logger
+      Logger.new(log_dir)
+    end
+
+    def log_dir
+      if File.exist?('config/honobono.yml')
+        yaml = YAML.load(File.read('config/honobono.yml'))
+        yaml['log_dir']
+      else
+        '/tmp/honobono.log'
+      end
+    end
+
     def run
-      log_dir = if File.exist?('config/honobono.yml')
-                  yaml = YAML.load(File.read('config/honobono.yml'))
-                  yaml['log_dir']
-                else
-                  '/tmp/honobono.log'
-                end
-      logger = Logger.new(log_dir)
       logger.info "[start] rake #{ARGV[0]}"
       Rake.application.run
       logger.info "[end] rake #{ARGV[0]}"
